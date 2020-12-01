8328f06e841112ea8ced0ec41478535833b2d8af
Fabian Brash <fabianbrash@gmail.com>
2020-12-01 17:16:42 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function Home() {
+export default function Home({ launches }) {
+  //console.log('launches', launches);
   return (
     <div className={styles.container}>
       <Head>
@@ -11,42 +13,25 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         SpaceX Launches
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Latest launches from SpaceX
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {launches.map(launch => {
+             return (
+              <a key={launch.id} href={launch.links.video_link}className={styles.card}>
+              <h3>{launch.mission_name}</h3>
+             <p><strong>Launch Time: </strong>{new Date(launch.launch_date_local).toLocaleDateString("en-US")}</p>
+             <p><strong>Launch Site: </strong>{launch.launch_site.site_name_long}</p>
+             <p><strong>Rocket Name: </strong>{launch.rocket.rocket_name}</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+             )
+          })}
         </div>
       </main>
 
@@ -62,4 +47,42 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql',
+    cache: new InMemoryCache()
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      query GetLaunches {
+        launchesPast(limit: 20) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+        }
+      }
+    `
+  })
+
+  //console.log(data);
+  return {
+    props: {
+      launches: data.launchesPast
+    }
+  }
 }
